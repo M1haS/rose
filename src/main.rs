2c2b8712c26722af49a8048dba2f80e5271b928d
Mihail Daskalu <dascaluofficial@gmail.com>
2024-02-26 01:52:46 +0300

    Vga driver

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,17 @@
 use core::panic::PanicInfo;
 use x86_64::instructions::hlt;
 
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop { hlt() }
 }
 
-static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xf;
-        }
-    }
+    println!("Hello {}", "World!");
+    panic!();
     loop { hlt() }
 }
